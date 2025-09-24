57f1898a869fd05fef6b1ea031bb9c07c9051168
Aditya Potnis <adipotnis@gmail.com>
2025-09-24 01:22:02 -0500

    bug fix: missing sourcing setup.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,24 @@ source /opt/ros/foxy/setup.bash
 # Source the built workspace (if it exists)
 if [ -f /workspace/install/local_setup.bash ]; then
     source /workspace/install/local_setup.bash
+    echo "WS found!"
 else
     echo "WS not found!"
 fi
 
+if [ -f /workspace/ros1_ws/devel/local_setup.bash ]; then
+    source /workspace/ros1_ws/devel/local_setup.bash
+    echo "ros1 custom msgs WS found!"
+else
+    echo "ros1 custom msgs WS not found!"
+fi
+
+if [ -f /workspace/ros2_ws/install/local_setup.bash ]; then
+    source /workspace/ros2_ws/install/local_setup.bash
+    echo "ros2 custom msgs WS found!"
+else
+    echo "ros2 custom msgs WS not found!"
+fi
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 # Execute the command passed to the container
